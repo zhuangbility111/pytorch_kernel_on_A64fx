d2304bf3255d2ba819002a9602f6bc765d971125
Ubuntu <ubuntu@ip-172-31-14-165.ap-northeast-1.compute.internal>
2023-08-25 11:41:27 +0000

    add fusion kernel

--- spmm_for_transposed_kernel/spmm_mm_fusion_kernel.cpp
+++ spmm_for_transposed_kernel/spmm_mm_fusion_kernel.cpp
@@ -4,5 +4,4 @@ std::tuple<torch::Tensor, torch::optional<torch::Tensor>>
 spmm_cpu_optimized(torch::Tensor rowptr, torch::Tensor col,
                    torch::optional<torch::Tensor> optional_value, torch::Tensor mat,
 				   int64_t sparse_rows, int64_t tile_num, std::string reduce) {
-
 }
